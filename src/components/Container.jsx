496953cd3f6f34ebac13d299444fd2c64cd4bdeb
Jamal Salim <Jamalsalim.js12@gmail.com>
2024-04-24 00:02:44 +0000

    Initial Commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Stats from "./Stats";
+import TextArea from "./TextArea";
+import {
+  INSTAGRAM_MAX_CHARACTERS,
+  FACEBOOK_MAX_CHARACTERS,
+} from "../lib/constants";
+
+const Container = () => {
+  const [text, setText] = useState("");
+  const stats = {
+    numberOfWords: text.split(/\s/).filter((word) => word !== "").length,
+    numberOfCharacters: text.length,
+    instagramCharactersLeft: INSTAGRAM_MAX_CHARACTERS - text.length,
+    facebookCharactersLeft: FACEBOOK_MAX_CHARACTERS - text.length,
+  };
+
+  return (
+    <main className="container">
+      <TextArea text={text} setText={setText} />
+      <Stats stats={stats} />
+    </main>
+  );
+};
+
+export default Container;
---
+++ src/components/TextArea.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
+const TextArea = ({ text, setText }) => {
+  const [warningText, setWarningText] = useState("");
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    if (newText.includes("<script>")) {
+      setWarningText("No script tag allowed");
+      newText = newText.replace("<script>", "");
+    } else if (newText.includes("@")) {
+      setWarningText("No @ symbol allowed");
+      newText = newText.replace("@", "");
+    } else {
+      setWarningText("");
+    }
+    setText(newText);
+  };
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+      />
+      {warningText ? <Warning warningText={warningText} /> : null}
+    </div>
+  );
+};
+
+export default TextArea;
